dbe0d6fbdd622ea9be7ca05dff13364d4fa91ec0
JoshDeCuir <joshmdecuir@gmail.com>
2024-02-21 20:54:51 -0600

    3.5 Exercise

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,16 +1,33 @@
 import { useEffect, useState } from 'react';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView} from '../movie-view/movie-view';
+import { LoginView } from '../login-view/login-view';
+import { SignupView } from '../signup-view/signup-view';
 
 export const MainView = () => {
+  const storedUser = JSON.parse(localStorage.getItem('user'));
+  const storedToken = localStorage.getItem('token');
+  const [user, setUser] = useState(storedUser? storedUser : null);
+  const [token, setToken] = useState(storedToken? storedToken : null);
   const [movies, setMovies] = useState([]);
-
   const [selectedMovie, setSelectedMovie] = useState(null);
 
+  const handleLogout = () => {
+    setUser(null);
+    setToken(null);
+    localStorage.clear();
+  };
+
   useEffect(() =>{
-    fetch('https://movieapi-ba6f568c0d4b.herokuapp.com/movies')
+    if (!token) {
+      return;
+    }
+    fetch('https://movieapi-ba6f568c0d4b.herokuapp.com/movies', {
+      headers: { Authorization: `Bearer ${token}` }
+    })
     .then((response) => response.json())
     .then((data) => {
+        console.log(data);
       const moviesFromApi = data.map((doc) => {
         return {
           id: doc._id,
@@ -29,7 +46,22 @@ export const MainView = () => {
       });
       setMovies(moviesFromApi);
     });
-  }, []);
+  }, [token]);
+
+  if (!user) {
+    return (
+    <> 
+    <LoginView 
+    onLoggedIn={(onLoggedInUser, loggedInToken) => {
+      setUser(onLoggedInUser)
+      setToken(loggedInToken);
+    }} 
+    />
+    or
+    <SignupView />
+    </>
+    );
+  }
 
   if (selectedMovie) {
     return (
@@ -43,6 +75,7 @@ export const MainView = () => {
 
   return (
     <div>
+      <button onClick={handleLogout}>Logout</button>
       {movies.map((movie) => (
         <MovieCard
           key={movie.id}
