3ca3e35411ef8c83068259bc64e5006852a74caa
JoshDeCuir <joshmdecuir@gmail.com>
2024-02-27 21:23:54 -0600

    3.7.2_exercise

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,12 +3,12 @@ import { MovieCard } from '../movie-card/movie-card';
 import { MovieView} from '../movie-view/movie-view';
 import { LoginView } from '../login-view/login-view';
 import { SignupView } from '../signup-view/signup-view';
+import { ProfileView } from '../profile-view/profile-view';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { NavigationBar } from '../../navigation-bar/navigation-bar';
-import { set } from 'mongoose';
 
 export const MainView = () => {
   const storedUser = JSON.parse(localStorage.getItem('user'));
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,9 +1,70 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Card } from 'react-bootstrap';
+import { Button, Card, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
-export const MovieCard = ({ movie }) => {
+export const MovieCard = ({ movie, token, setUser, user }) => {
+
+  const [isFavorite, setIsFavorite] = useState(
+    false
+  );
+
+  useEffect(() => {
+    if(user.favoriteMovies && user.favoriteMovies.includes(movie.id)) {
+      setIsFavorite(true);
+    }
+  }, [user]);
+
+  const addFavoriteMovie = () => {
+    fetch(`https://movieapi-ba6f568c0d4b.herokuapp.com/users/${user.username}/movies/${movie.id}`,
+    { method: 'POST', headers: {Authorization: `Bearer ${token}`}}
+    )
+      .then((response) => {
+        if(response.ok){
+          return response.json();
+        } else {
+          console.log('Failed to add favorite movie');
+        }
+      })
+      .then((user) => {
+        if (user) {
+          alert('successfully added to favorites');
+          localStorage.setItem('user', JSON.stringify(user));
+          setUser(user);
+          setIsFavorite(true);
+        }
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  };
+
+  const removeFavoriteMovie = () => {
+    fetch(`https://movieapi-ba6f568c0d4b.herokuapp.com/users/${user.username}/movies/${movie.id}`,
+    {method: 'DELETE', headers:{Authorization: `Bearer ${token}`}}
+    )
+
+    .then((response) => {
+      if(response.ok) {
+        return response.json();
+      } else {
+        alert('Failed');
+      }
+    })
+    .then((user) => {
+      if(user) {
+        alert('Successfully deleted from favories');
+        localStorage.setItem('user', JSON.stringify(user));
+        setUser(user);
+        setIsFavorite(false);
+      }
+    })
+    .catch((error) => {
+      alert(error);
+    });
+  };
+
   return (
     <Card className='h-100'>
       <Card.Img variant='top' src={movie.image} />
@@ -12,7 +73,15 @@ export const MovieCard = ({ movie }) => {
         <Card.Text>{movie.director.name}</Card.Text>
         <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
           <Button variant='link'>Open</Button>
-        </Link>  
+        </Link>
+
+        <Card.Body className='favorite-btns'>
+          {!isFavorite ? (
+            <Button className='fav-btn' onClick={addFavoriteMovie}>+</Button>
+          ): (
+            <Button className='fav-btn' onClick={removeFavoriteMovie}>-</Button>
+          )}
+          </Card.Body>  
       </Card.Body>
     </Card>
   );
